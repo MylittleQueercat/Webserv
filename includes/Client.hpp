0302d1bcf1fde1867e17be27d3bdc8bec0818bf3
lenovo <wangushaonv@gmail.com>
2026-04-26 17:04:19 +0200

    old_version

--- includes/Client.hpp
+++ includes/Client.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Client.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hguo <hguo@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: jili <jili@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/30 17:03:34 by jili              #+#    #+#             */
-/*   Updated: 2026/04/07 16:51:56 by hguo             ###   ########.fr       */
+/*   Updated: 2026/03/30 17:03:35 by jili             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,6 @@ struct ClientState {
     int         fd;
     std::string recv_buffer;    // accumulated received data
     std::string send_buffer;    // data waiting to be sent
-    std::string cgi_body_buffer;  // accumulates raw chunked body until 0\r\n\r\n
     bool        headers_done;   // have the headers been fully read?
     size_t      content_length; // length of the request body
     ServerConfig *config;       // points to the server config for this client
@@ -31,7 +30,6 @@ struct ClientState {
     // CGI
     pid_t       cgi_pid;
     int         cgi_output_fd;// Source: the pipe the server reads raw bytes from CGI process
-    int         cgi_input_fd;
     bool        is_cgi;
     std::string cgi_output;//Destination: the string stores the raw bytes
     time_t      cgi_last_activity;
