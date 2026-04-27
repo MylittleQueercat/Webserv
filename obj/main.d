8e9ae7bfd4644f71545aa97e5ec0bd560dc7ac8d
huilin guo <hguo@f6r13s10.paris.42.school>
2026-04-27 16:47:13 +0200

    rm comments

--- obj/main.d
+++
@@ -1,9 +0,0 @@
-obj/main.o: main.cpp includes/Webserv.hpp includes/Client.hpp \
- includes/ConfigParser.hpp includes/Http.hpp includes/CGI.hpp \
- includes/Server.hpp
-includes/Webserv.hpp:
-includes/Client.hpp:
-includes/ConfigParser.hpp:
-includes/Http.hpp:
-includes/CGI.hpp:
-includes/Server.hpp:
